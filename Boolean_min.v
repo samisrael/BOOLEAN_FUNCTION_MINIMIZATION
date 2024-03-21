c5df8fb4441973ecfb915b9edfa9021f937e7d13
samisrael <samisrael0407@gmail.com>
2024-03-21 16:23:49 +0530

    final

--- Boolean_min.v
+++ Boolean_min.v
@@ -15,5 +15,10 @@ and(p,bdash,ddash);
 and(q,adash,b,d);
 and(r,a,b,cdash);
 or(f1,p,q,r);
-//type code for f2 as like f1
+
+not(ybar, y);
+and(s, ybar,z);
+and(t,x,y);
+and(u,w,y);
+or(f2,s,t,u);
 endmodule
